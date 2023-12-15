f6d4f65919438b01e3a9723c28d95e90f68ba4c3
saijo-108 <toyatoyasj@gmail.com>
2023-12-15 16:43:34 +0900

    ファイル名変更

--- app/src/task/index.php
+++ app/src/task/index.php
@@ -1,15 +1,18 @@
+<?php 
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>task TOP</title>
+    <title>task</title>
   </head>
   <body>
-    <h1>My Task</h1>
+    <h1>タスク作成</h1>
     <ul>
       <li>Task 1</li>
-      <li>Task 2</li>
+      <li>Task 2aaa</li>
       <!-- 他のタスクをここに追加 -->
     </ul>
   </body>
